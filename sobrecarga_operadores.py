61e1c06b978af3d95546cae811286f9da07de1bd
juan pablo ruiz <127860340+jpruizf@users.noreply.github.com>
2023-05-03 15:45:54 -0300

    Ejercicio 6
    Clase ViajeroFrecuente - sobre carga de operadores

---
+++ sobrecarga_operadores.py
@@ -0,0 +1,28 @@
+#Defino clase Viajeros Frecuentes
+
+class ViajeroFrecuente:
+    __numero: int
+    __dni: str
+    __nombre: str
+    __apellido: str
+    __acum_millas: int
+   
+    #Definicion del constructor
+    
+    def __init__(self,num,dni,nom,ape,acum_millas):
+        self.__numero = num
+        self.__dni = dni
+        self.__nombre = nom
+        self.__apellido = ape
+        self.__acum_millas = acum_millas
+    def getdatos(self):
+        return print(f"Bienvenido viajero numero: {self.__numero} | DNI Ingresado: {self.__dni} | Estimado: {self.__nombre}{self.__apellido} | Tiene acumulado un total de {self.__acum_millas} millas")
+    
+    def __gt__(self,otro_viajero: int):
+        return self.__acum_millas > otro_viajero
+    def __ad__(self,v):
+        v = v + 100
+        return v
+    def __sub__(self,v):
+        v = v-100
+        return v
